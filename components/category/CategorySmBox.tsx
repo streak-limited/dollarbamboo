4fd1fe95f732487183c73d57bfa449d7e8e43b99
Kwok Wing Cheung Dicky <dicky1130630@gmail.com>
2023-10-20 20:21:37 +0800

    ok

--- components/category/CategorySmBox.tsx
+++ components/category/CategorySmBox.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
-import { useLanguage } from '../../hooks/useLanguage'
 import Link from 'next/link'
+import { useLanguage } from '../../hooks/useLanguage'
 
 interface Props {
   imgSrc: string
@@ -18,28 +18,30 @@ const CategorySmBox: React.FC<Props> = ({
   const { t, locale } = useLanguage()
   return (
     <Link href={`${href}`}>
-      <div
-        className={`flex flex-col items-center text-center  ${
-          locale === 'en'
-            ? 'w-[10rem] sm:w-[13rem]'
-            : 'min-w-[7rem] w-[9.3rem] sm:w-[10rem]'
-        } my-2`}
-      >
+      <a>
         <div
-          className={`flex items-center justify-center w-[60px] h-[60px] rounded-full bg-palette-${bgc}`}
+          className={`flex flex-col items-center text-center  ${
+            locale === 'en'
+              ? 'w-[10rem] sm:w-[13rem]'
+              : 'min-w-[7rem] w-[9.3rem] sm:w-[10rem]'
+          } my-2`}
         >
-          <Image
-            src={`/images/category-icon/${imgSrc}`}
-            alt={categoryTitle}
-            width={45}
-            height={45}
-            className="drop-shadow-lg"
-          />
+          <div
+            className={`flex items-center justify-center w-[60px] h-[60px] rounded-full bg-palette-${bgc}`}
+          >
+            <Image
+              src={`/images/category-icon/${imgSrc}`}
+              alt={categoryTitle}
+              width={45}
+              height={45}
+              className="drop-shadow-lg"
+            />
+          </div>
+          <h3 className="text-sm md:text-base font-bold mt-2">
+            {t[`${categoryTitle}`]}
+          </h3>
         </div>
-        <h3 className="text-sm md:text-base font-bold mt-2">
-          {t[`${categoryTitle}`]}
-        </h3>
-      </div>
+      </a>
     </Link>
   )
 }
