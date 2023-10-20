0d9ab2964a6262334cc122eb899df3469be7cd83
Kwok Wing Cheung Dicky <dicky1130630@gmail.com>
2023-10-20 20:15:56 +0800

    dev

--- components/category/CategorySmBox.tsx
+++ components/category/CategorySmBox.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { useLanguage } from "../../hooks/useLanguage";
+import React from 'react'
+import Image from 'next/image'
+import { useLanguage } from '../../hooks/useLanguage'
+import Link from 'next/link'
 
 interface Props {
-  imgSrc: string;
-  bgc: string;
-  categoryTitle: string;
-  href: string;
+  imgSrc: string
+  bgc: string
+  categoryTitle: string
+  href: string
 }
 const CategorySmBox: React.FC<Props> = ({
   imgSrc,
@@ -15,35 +15,33 @@ const CategorySmBox: React.FC<Props> = ({
   categoryTitle,
   href,
 }) => {
-  const { t, locale } = useLanguage();
+  const { t, locale } = useLanguage()
   return (
     <Link href={`${href}`}>
-      <a>
+      <div
+        className={`flex flex-col items-center text-center  ${
+          locale === 'en'
+            ? 'w-[10rem] sm:w-[13rem]'
+            : 'min-w-[7rem] w-[9.3rem] sm:w-[10rem]'
+        } my-2`}
+      >
         <div
-          className={`flex flex-col items-center text-center  ${
-            locale === "en"
-              ? "w-[10rem] sm:w-[13rem]"
-              : "min-w-[7rem] w-[9.3rem] sm:w-[10rem]"
-          } my-2`}
+          className={`flex items-center justify-center w-[60px] h-[60px] rounded-full bg-palette-${bgc}`}
         >
-          <div
-            className={`flex items-center justify-center w-[60px] h-[60px] rounded-full bg-palette-${bgc}`}
-          >
-            <Image
-              src={`/images/category-icon/${imgSrc}`}
-              alt={categoryTitle}
-              width={45}
-              height={45}
-              className="drop-shadow-lg"
-            />
-          </div>
-          <h3 className="text-sm md:text-base font-bold mt-2">
-            {t[`${categoryTitle}`]}
-          </h3>
+          <Image
+            src={`/images/category-icon/${imgSrc}`}
+            alt={categoryTitle}
+            width={45}
+            height={45}
+            className="drop-shadow-lg"
+          />
         </div>
-      </a>
+        <h3 className="text-sm md:text-base font-bold mt-2">
+          {t[`${categoryTitle}`]}
+        </h3>
+      </div>
     </Link>
-  );
-};
+  )
+}
 
-export default CategorySmBox;
+export default CategorySmBox
--- lib/client.ts
+++ lib/client.ts
@@ -1,16 +1,16 @@
-import sanityClient from "@sanity/client";
-import imageUrlBuilder from "@sanity/image-url";
+import sanityClient from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
 
 //connect to sanity
 export const client = sanityClient({
-  projectId: "3c4n15ly",
-  dataset: "production",
-  apiVersion: "2022-06-04",
+  projectId: 'n2ivwii9',
+  dataset: 'production',
+  apiVersion: '2022-06-04',
   useCdn: true,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
-});
+})
 
 //be able to use sanity images
-const builder = imageUrlBuilder(client);
+const builder = imageUrlBuilder(client)
 
-export const urlFor = (source:any) => builder.image(source)
+export const urlFor = (source: any) => builder.image(source)
