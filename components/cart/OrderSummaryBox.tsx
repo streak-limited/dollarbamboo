fb5eff1b77c303205061a2a4d7faa4321e9dc3c8
Kwok Wing Cheung Dicky <dicky1130630@gmail.com>
2024-03-14 19:30:56 +0800

    a

--- components/cart/OrderSummaryBox.tsx
+++ components/cart/OrderSummaryBox.tsx
@@ -46,7 +46,7 @@ const OrderSummaryBox = () => {
     onSuccess: async () => {
       // toast.success(TEXT.TOAST_SUCCESS_EDIT_ADDRESS);
       // push(Paths.ACCOUNT_ADDRESS_BOOK);
-      // selectedShopRefetch();
+      // selectedShopRefetch(); a
     },
     onError: (error: Error) => {
       toast.error(error.message)
